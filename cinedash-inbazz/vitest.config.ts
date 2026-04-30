9933fd2f98aefacc0cec2e22d1ee9e7299b030cf
Vitor Augusto <95655990+VitorAam@users.noreply.github.com>
2026-04-30 01:01:20 -0300

    [FIX] Pequenos ajustes de pastas

--- cinedash-inbazz/vitest.config.ts
+++ cinedash-inbazz/vitest.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: ['./src/test/setup.ts'],
+        setupFiles: ['./src/shared/lib/setup.ts'],
         css: false,
         clearMocks: true,
         restoreMocks: true,
